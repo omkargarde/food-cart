d465f6e3dead776f67a475b83779434d4d2897a8
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-06-23 19:43:03 +0530

    constants clean up

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,7 +6,5 @@ export const LOGO_URL: string = "/src/assets/smoking-burger-with-lettuce-3624ld.
 export const SWIGGY_API_URL_FOR_HOMEPAGE =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-// export const SWIGGY_API_URL_FOR_PRODUCT_PAGE =
-//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=10575&catalog_qa=undefined&submitAction=ENTER";
 export const SWIGGY_API_URL_FOR_PRODUCT_PAGE =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
